1e7057be2604f62932759b376dca1a138bdfbf45
Issa Boubacar <bacaris78@gmail.com>
2018-03-21 16:33:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle){
  return playlist 
 }
 function removeFromPlaylist(playlist, artistName){
- var updatePlaylist = delete artistName.playlist;
+ var updatePlaylist = delete playlist[artistName];
  return updatePlaylist
 }
